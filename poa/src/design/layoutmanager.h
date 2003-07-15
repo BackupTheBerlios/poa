754d9613ff7445d2c071b1c82d5c00a65c97efe6
keulsn <keulsn>
2003-07-15 09:20:13 +0000

    copy_n_past fix

--- poa/src/design/layoutmanager.h
+++ poa/src/design/layoutmanager.h
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: layoutmanager.h,v 1.1 2003/07/14 19:52:42 garbeam Exp $
+ * $Id: layoutmanager.h,v 1.2 2003/07/15 09:20:13 keulsn Exp $
  *
  *****************************************************************************/
 
@@ -35,7 +35,7 @@ class LayoutManager
   public:
 
     LayoutManager();
-    virtual ~CodeManager();
+    virtual ~LayoutManager();
 
 };
 

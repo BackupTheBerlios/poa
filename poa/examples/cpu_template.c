3d5d2fc4d0a44b61f190a3c231dba79467fc7e6d
garbeam <garbeam>
2003-09-17 13:18:32 +0000

    added hello world template

---
+++ poa/examples/cpu_template.c
@@ -0,0 +1,13 @@
+/**
+ * POA CPU $$NR.
+ *
+ * $Id: cpu_template.c,v 1.1 2003/09/17 13:18:32 garbeam Exp $
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("This is POA CPU $$NR.\n");
+    return 0;
+}

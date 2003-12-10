07bc566ff8fd8bcea50e8352858ce61eb1fa91f3
squig <squig>
2003-12-10 14:46:51 +0000

    generalized makefile

--- poa/test-src/main.cpp
+++ poa/test-src/main.cpp
@@ -1,15 +1,15 @@
-#include <cppunit/extensions/TestFactoryRegistry.h> 
-#include <cppunit/ui/text/TestRunner.h> 
-#include <cppunit/CompilerOutputter.h> 
-#include <cppunit/TextOutputter.h> 
-#include <cppunit/XmlOutputter.h> 
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/XmlOutputter.h>
 #include <iostream>
 
-/**  Main class for all the CppUnit test classes  
+/**  Main class for all the CppUnit test classes
 *
 *  This will be the driver class of all your CppUnit test classes.
-*  - All registered CppUnit test classes will be run. 
-*  - You can also modify the output (text, compiler, XML). 
+*  - All registered CppUnit test classes will be run.
+*  - You can also modify the output (text, compiler, XML).
 *  - This class will also integrate CppUnit test with Oval
 */
 
@@ -23,24 +23,24 @@ int main( int argc, char **argv)
    CppUnit::TextUi::TestRunner runner;
    runner.addTest( suite );
 
-   // Change the default outputter to a compiler error format outputter 
+   // Change the default outputter to a compiler error format outputter
    // uncomment the following line if you need a compiler outputter.
    //runner.setOutputter(new CppUnit::CompilerOutputter( &runner.result(),
    //                                                     std::cerr ) );
 
-   // Change the default outputter to a xml error format outputter 
+   // Change the default outputter to a xml error format outputter
    // uncomment the following line if you need a xml outputter.
    //runner.setOutputter( new CppUnit::XmlOutputter( &runner.result(),
    //                                                    std::cerr ) );
 
    /// Run the tests.
        bool wasSuccessful = runner.run();
-   // If you want to avoid the CppUnit typical output change the line above 
-   // by the following one: 
+   // If you want to avoid the CppUnit typical output change the line above
+   // by the following one:
    //  bool wasSucessful = runner.run("",false,false,false);
 
    // Return error code 1 if the one of test failed.
    // Uncomment the next line if you want to integrate CppUnit with Oval
    //  std::cout <<"[OVAL] Cppunit-result ="<<!wasSuccessful<<"\n" ;
-     return 0;
+     return (wasSuccessful) ? 0 : 1;
  }

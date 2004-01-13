e48577c53f94a412583702a6ce3c4f9a87a0b521
squig <squig>
2004-01-13 00:28:59 +0000

    refactored PoaException to suite test case

---
+++ poa/src/poaexception.cpp
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ * POA - Programmierumgebung Offener Antrieb
+ *
+ * Copyright (C) 2003 Necati Aydin, Anselm Garbe, Stefan Hauser,
+ * Steffen Keul, Marcel Kilgus, Steffen Pingel, Tammo van Lessen
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id: poaexception.cpp,v 1.1 2004/01/13 00:28:59 squig Exp $
+ *
+ *****************************************************************************/
+
+#include "poaexception.h"
+
+PoaException::PoaException(const QString& msg)
+    : msg_( msg )
+{
+}
+
+QString PoaException::message() const
+{
+    return msg_;
+}

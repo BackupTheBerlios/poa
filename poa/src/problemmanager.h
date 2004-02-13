d7264cfa3a392f8e1c56914512ff7929d3ac82df
squig <squig>
2004-02-13 15:57:57 +0000

    added comments

--- poa/src/problemmanager.h
+++ poa/src/problemmanager.h
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: problemmanager.h,v 1.3 2004/01/19 15:25:40 squig Exp $
+ * $Id: problemmanager.h,v 1.4 2004/02/13 15:57:57 squig Exp $
  *
  *****************************************************************************/
 
@@ -33,6 +33,10 @@ class QWidget;
 #include "pinmodel.h"
 #include "project.h"
 
+/**
+ * Stores a problem report. A problem report can support quick fix
+ * actions to solve the problem.
+ */
 class ProblemReportItem : public QObject, public QListViewItem
 {
     Q_OBJECT
@@ -48,12 +52,47 @@ public:
      */
     virtual void addWidgets(QWidget *widget);
 
+    /**
+     * Returns true, if the problem has been fixed.
+     */
     bool isFixed() const;
+
+    /**
+     * Returns a long description that is shown when the report is
+     * selected by the user.
+     */
     QString longDescription() const;
+
+    /**
+     * Sets the long description.
+     *
+     * @see #longDescription()
+     */
     void setLongDescription(const QString longDescription);
+
+    /**
+     * Sets the short description.
+     *
+     * @see #shortDescription()
+     */
     void setShortDescription(const QString shortDescription);
+
+    /**
+     * Sets the status. This should be either "Warning" or "Critical".
+     *
+     * @see #status()
+     */
     void setStatus(const QString status);
+
+    /**
+     * Returns the short description. The short description is
+     * displayed in the first column of the list view.
+     */
     QString shortDescription() const;
+
+    /**
+     * Returns the status
+     */
     QString status() const;
 
 signals:
@@ -62,6 +101,9 @@ signals:
 
 protected:
 
+    /**
+     * Updates the fixed status and emits the updated signal.
+     */
     void setFixed(const bool fixed);
 
 private:
@@ -70,12 +112,20 @@ private:
     QString longDescription_;
 };
 
+/**
+ * Represents a reports for a pin that is not connected.
+ */
 class DisconnectedPinReport : public ProblemReportItem
 {
     Q_OBJECT
 
 public:
 
+    /**
+     * Constructs the report.
+     *
+     * @param the disconnected pin
+     */
     DisconnectedPinReport(QListViewItem *parent, PinModel *pin);
 
     virtual void addWidgets(QWidget *widget);
@@ -90,12 +140,21 @@ private:
 
 };
 
+/**
+ * Represents a reports for a two connected pins that have different widths.
+ */
 class DifferentWidthReport : public ProblemReportItem
 {
     Q_OBJECT
 
 public:
 
+    /**
+     * Constructs the report.
+     *
+     * @param source the first pin
+     * @param target the second pin
+     */
     DifferentWidthReport(QListViewItem *parent, PinModel *source,
                          PinModel *target);
 
@@ -113,12 +172,21 @@ private:
 
 };
 
+/**
+ * Represents a reports for a two connected blocks that have different clocks.
+ */
 class DifferentClockReport : public ProblemReportItem
 {
     Q_OBJECT
 
 public:
 
+    /**
+     * Constructs the report.
+     *
+     * @param source the first block
+     * @param target the second block
+     */
     DifferentClockReport(QListViewItem *parent, BlockModel *source,
                          BlockModel *target);
 
@@ -137,20 +205,40 @@ private:
 };
 
 /**
- * Checks a project for consistency
+ * Checks a project for consistency.
  */
 class ProblemManager
 {
 public:
 
+    /**
+     * Constructs a problem manager.
+     *
+     * @param project the project to check
+     * @param listView problem reports are appended to listView
+     */
     ProblemManager(Project *project, QListView *listView);
     ~ProblemManager();
 
+    /**
+     * Check the project and append the reports to the list
+     * view. Problem reports are grouped into categories and their
+     * status is represented by an icon.
+     *
+     * @see ProblemReportItem
+     */
     void report();
 
 protected:
 
+    /**
+     * Checks block for problems.
+     */
     void checkBlock(BlockModel *block);
+
+    /**
+     * Updates the comment field of the item.
+     */
     void updateRoot(QListViewItem *item);
 
 private:

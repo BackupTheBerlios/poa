f817f7e446aee4ba39e18bc8dd8f36406764423c
kilgus <kilgus>
2004-06-03 13:28:38 +0000

    Small fixes

--- poa/src/codemanager.cpp
+++ poa/src/codemanager.cpp
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: codemanager.cpp,v 1.22 2004/02/11 13:38:48 garbeam Exp $
+ * $Id: codemanager.cpp,v 1.23 2004/06/03 13:28:38 kilgus Exp $
  *
  *****************************************************************************/
 
@@ -201,13 +201,13 @@ void CodeManager::substitute() {
          it != pins.end(); ++it)
     {
         PinModel *pin = *it;
-        substTxt.append(QString("    np_pio *%1 = 0x%2;\n")
+        substTxt.append(QString("    np_pio *%1 = (np_pio*) 0x%2;\n")
                             .arg(pin->name())
-                            .arg(pin->address()));
+                            .arg(pin->address(), 0, 16));
     }
 
     QString source = sourceCode();
-    // Note: fucking QRegExp don't works over newlines, so
+    // Note: fucking QRegExp doesn't work over newlines, so
     // we've to double-match... - so QRegExp is useless indeed :((
     int firstIndex = source.find("/*!POA!*/");
     int lastIndex = source.find("/*!AOP!*/");

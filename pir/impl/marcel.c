3e887799c9ce015396c29c66eae920820ab76a1a
keulsn <keulsn>
2004-12-06 14:39:43 +0000

    removed tabs

--- pir/impl/marcel.c
+++ pir/impl/marcel.c
@@ -27,29 +27,29 @@ int main( int argc, char** argv )
     if( (image[1] = cvLoadImage( filename2, 1)) == 0 )
         return -1;
 
-	source[0] = cvCreateImage(cvGetSize(image[0]), IPL_DEPTH_8U, 1);
-	source[1] = cvCreateImage(cvGetSize(image[1]), IPL_DEPTH_8U, 1);
-	cvCvtColor(image[0], source[0], CV_BGR2GRAY);
-	cvCvtColor(image[1], source[1], CV_BGR2GRAY);
+    source[0] = cvCreateImage(cvGetSize(image[0]), IPL_DEPTH_8U, 1);
+    source[1] = cvCreateImage(cvGetSize(image[1]), IPL_DEPTH_8U, 1);
+    cvCvtColor(image[0], source[0], CV_BGR2GRAY);
+    cvCvtColor(image[1], source[1], CV_BGR2GRAY);
 
-	cvNamedWindow("Pic1", 0);
+    cvNamedWindow("Pic1", 0);
     cvShowImage("Pic1", source[0]);
 
     cvNamedWindow("Pic2", 0);
     cvShowImage("Pic2", source[1]);
 
-	size = cvGetSize(source[0]);
+    size = cvGetSize(source[0]);
 
-	size.height /= 5;
-	size.width /= 5;
+    size.height /= 5;
+    size.width /= 5;
 
     velocityX = cvCreateImage(size, IPL_DEPTH_32F, 1);
     velocityY = cvCreateImage(size, IPL_DEPTH_32F, 1);
 
-//	cvCalcOpticalFlowLK(source[0], source[1], cvSize(3, 3), velocityX, velocityY);
-	cvCalcOpticalFlowBM(source[0], source[1], cvSize(5, 5), cvSize(1, 1), cvSize(1, 1), 0, velocityX, velocityY);
+//  cvCalcOpticalFlowLK(source[0], source[1], cvSize(3, 3), velocityX, velocityY);
+    cvCalcOpticalFlowBM(source[0], source[1], cvSize(5, 5), cvSize(1, 1), cvSize(1, 1), 0, velocityX, velocityY);
 
-	cvNamedWindow("Horizontal", 0);
+    cvNamedWindow("Horizontal", 0);
     cvShowImage("Horizontal", velocityX);
 
     cvNamedWindow("Vertical", 0);
@@ -62,9 +62,9 @@ int main( int argc, char** argv )
     cvDestroyWindow("Horizontal");
     cvDestroyWindow("Vertical");
 
-	cvReleaseImage(&source[1]);
+    cvReleaseImage(&source[1]);
     cvReleaseImage(&source[0]);
-	cvReleaseImage(&image[1]);
+    cvReleaseImage(&image[1]);
     cvReleaseImage(&image[0]);
     cvReleaseImage(&velocityY);
     cvReleaseImage(&velocityX);
